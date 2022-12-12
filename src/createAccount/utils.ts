1628d536a84beea1298ad443d4b13cfe40bd793c
Yoni Kosiner <yoni@kosiner.co.uk>
2022-12-12 01:28:06 +0000

    why the fuck won't it work :(

--- src/createAccount/utils.ts
+++ src/createAccount/utils.ts
@@ -2,8 +2,8 @@ import { Page } from "puppeteer";
 
 export function ClickElementByXPath(page: Page, xPaths: string[]): void {
     xPaths.map(async (path: string) => {
-        const button = await page.$x(path);
+        const button = await page.$x(`${path}`);
         //@ts-ignore
-        button[0].click();
+        await button[0].click();
     })
 }
